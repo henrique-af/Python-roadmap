82ad62b69536a5c5c99725b8e20a26597281823b
Henrique Albuquerque <henriquealbuquerquef@gmail.com>
2021-12-17 12:20:12 -0300

    final commit

--- learning.csv/project01/readingCsv.py
+++ learning.csv/project01/readingCsv.py
@@ -10,23 +10,24 @@ with open (filePath, encoding='utf8') as fileOpened:
         reader = csv.reader(fileOpened, delimiter=',')
         for row in reader:
                 data.append(row)
-                
+while True:            
 #qual nome deseja buscar?
-nomeBusca = input('Digite o nome que deseja encontrar: ')
-
+        nomeBusca = input('\nDigite o nome que deseja encontrar ou 0 para parar: ')
+        
+#condição de parada do loop
+        if nomeBusca == '0':
+                break 
+             
 #realiza a leitura da coluna 0, que são os nomes e armazena em data
-nomeColuna = [x[0] for x in data]
-
+        nomeColuna = [x[0] for x in data]
+                 
 # busca o nome na coluna nome
-if nomeBusca in nomeColuna:
-        for x in range(0, len(data)):
-                if nomeBusca == data[x][0]:
-                        time.sleep(0.1)
-                        # nome, sexo, endereco, cidade, email, telefone, idade = data[x].split(',')
-                        # print(nome)
-                        myList=','.join([str(item) for item in data[x]])
-                        myList.split(',')
-                        print(myList[1])
-                        # print('Nome: {}\nSexo: {}\nEndereço: {}\nCidade: {}\nE-mail: {}\nTelefone: {}\nIdade: {}'.format(myList[0], myList[1], myList[2], myList[3], myList[4], myList[5], myList[6]))
-else:
-        print("\nNome não encontrado, tente novamente.")
+        if nomeBusca in nomeColuna:
+                for x in range(0, len(data)):
+                        if nomeBusca == data[x][0]:
+                                time.sleep(0.6)
+                                myList=','.join([str(item) for item in data[x]])
+                                myList_ = myList.split(',')
+                                print('\nNome: {}\nSexo: {}\nEndereço: {}\nCidade: {}\nE-mail: {}\nTelefone: {}\nIdade: {}'.format(myList_[0], myList_[1], myList_[2], myList_[3], myList_[4], myList_[5], myList_[6])) 
+        else:
+                        print("\nNome não encontrado, tente novamente.")
